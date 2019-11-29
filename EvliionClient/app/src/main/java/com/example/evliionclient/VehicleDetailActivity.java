922f2135137c84f735b09764a8eb1743ced3feb3
DESKTOP-SH958UQ\DELL <3754311+lonewolf9277@users.noreply.github.com>
2019-11-30 03:47:24 +0500

    Added vehicle details activity.

--- EvliionClient/app/src/main/java/com/example/evliionclient/VehicleDetailActivity.java
+++ EvliionClient/app/src/main/java/com/example/evliionclient/VehicleDetailActivity.java
@@ -1,6 +1,5 @@
 package com.example.evliionclient;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -10,24 +9,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
-public class ProfileActivity extends AppCompatActivity {
+public class VehicleDetailActivity extends AppCompatActivity {
 
-    LinearLayout vehicleDetails;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_vehicle_detail);
 
-        vehicleDetails =  findViewById(R.id.vehicleDetails);
-
-        vehicleDetails.setOnClickListener(new View.OnClickListener() {
+        Button editVehicleBtn = (Button) findViewById(R.id.editVehicleBtn);
+        editVehicleBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ProfileActivity.this, VehicleDetailActivity.class);
-                startActivity(intent);
-
+                //TODO
             }
         });
 
