51088004c3c3856818ba573edc867cddba25a45a
DESKTOP-SH958UQ\DELL <3754311+lonewolf9277@users.noreply.github.com>
2019-12-13 05:05:38 +0500

    added start charging screen

---
+++ EvliionClient/app/src/main/java/com/example/evliionclient/StartChargingActivity.java
@@ -0,0 +1,14 @@
+package com.example.evliionclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartChargingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_charging);
+    }
+}
